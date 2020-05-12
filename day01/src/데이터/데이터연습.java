a801c9dd88fa3bd1a05abfc9ad28edbc250d78b2
404-Not-Found-345 <50369738+404-Not-Found-345@users.noreply.github.com>
2020-05-12 22:16:44 +0900

    Add files via upload

---
+++ day01/src/데이터/데이터연습.java
@@ -0,0 +1,11 @@
+package 데이터;
+
+public class 데이터연습 {
+
+	public static void main(String[] args) {
+		// comment, 설명 / 주석 단축키: ctrl + / or ctrl + alt + /
+		System.out.println("안녕하세요."); //단축키: sysout + ctrl + space
+		//run: f11
+	}
+
+}
